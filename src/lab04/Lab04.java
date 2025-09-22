ffeb1d0b9653f7040885babc43c374bd3b65389c
2494377 <2494377@host.docker.internal>
2025-09-22 09:26:15 -0400

    changed background color, changed label style, added button

--- src/lab04/Lab04.java
+++ src/lab04/Lab04.java
@@ -7,10 +7,12 @@ package lab04;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -27,7 +29,7 @@ public class Lab04 extends Application {
     public void start(Stage primaryStage) { 
         primaryStage.setTitle("JavaFX");
         BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 400, 400);
+        Scene scene = new Scene(root, 450, 500);
         
         GridPane grid = new GridPane();
         grid.setHgap(10);
@@ -35,20 +37,27 @@ public class Lab04 extends Application {
         grid.setPadding(new Insets(10, 0, 0, 20));
         root.setCenter(grid);
         
+        Label money = new Label("$");
+        Button calculate = new Button("Calculate");
+        
         Label days = new Label("Number of days:");
         grid.add(days, 0, 1);
         TextField daysBox = new TextField();
         grid.add(daysBox, 1, 1);
+        Label daysTxt = new Label("days");
+        grid.add(daysTxt, 2, 1);
         
         Label airfare = new Label("Airfare amount:");
         grid.add(airfare, 0, 2);
         TextField airfareBox = new TextField();
         grid.add(airfareBox, 1, 2);
+        grid.add(money, 2, 2);
         
         Label carRental = new Label("Car renting amount:");
         grid.add(carRental, 0, 3);
         TextField carRentalBox = new TextField();
         grid.add(carRentalBox, 1, 3);
+//        grid.add(money, 2 , 3);
         
         Label milesDriven = new Label("Miles Driven:");
         grid.add(milesDriven, 0, 4);
@@ -76,6 +85,24 @@ public class Lab04 extends Application {
         TextField lodgingChargesBox = new TextField();
         grid.add(lodgingChargesBox, 1, 8);
         
+        Button clear = new Button("Clear");
+        clear.setOnAction(e -> {
+            daysBox.clear();
+            airfareBox.clear();
+            carRentalBox.clear();
+            milesDrivenBox.clear();
+            parkingFeesBox.clear();
+            taxiChargeBox.clear();
+            seminarRegistrationFeesBox.clear();
+            lodgingChargesBox.clear();
+        });
+        
+        HBox hbox1 = new HBox(10);
+        hbox1.getChildren().add(calculate);
+        hbox1.getChildren().add(clear);
+        grid.add(hbox1, 0, 9);
+        
+        scene.getStylesheets().add("mystyle.css");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
