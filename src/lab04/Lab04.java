3768908b9232ab6d2cfa0e0992a88b711fbf2b11
2494377 <2494377@host.docker.internal>
2025-09-22 08:20:18 -0400

    Initial Commit

---
+++ src/lab04/Lab04.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab04;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2494377
+ */
+public class Lab04 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        
+        primaryStage.setTitle("JavaFX");
+        primaryStage.show();
+    }
+}
